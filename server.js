4a2460c2d10d064874c279d6a7e675621ef1b3bf
SaxoDevs <112919860+saxodevs@users.noreply.github.com>
2026-01-01 15:15:52 +0000

    update server.js to restrict CORS to saxodevs.github.io domain

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://saxodevs.github.io",
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
